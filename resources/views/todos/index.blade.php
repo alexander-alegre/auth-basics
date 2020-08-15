f8ad0c3b0078e6b836ce9bbf6bad65e9ae13196f
denny <dmartinezv99@gmail.com>
2020-08-15 14:13:29 -0700

    added todo list to views

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -9,10 +9,17 @@
                 <label for="title">Title</label>
                 <input type="text" class="form-control" id="title" name="title">
                 @error('title')
-                    <p class="bg-danger text-white px-1 rounded mt-3 text-center">{{$message}}</p>
+                <p class="bg-danger text-white px-1 rounded mt-3 text-center">{{$message}}</p>
                 @enderror
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+
+        <ul class="list-group mt-3">
+            @foreach($todos as $todo)
+            <li class="list-group-item">{{$todo->title}}</li>
+            @endforeach
+        </ul>
+
     </div>
 @endsection
